1f7d4525b7bebc3c6ad2b6071da834cf03c01b80
Mahdizadeh <Amirali.mahdizadeh81@gmail.com>
2023-07-31 20:53:21 +0330

    add jason and retrofit library  and edit News class

--- app/src/main/java/com/newsapp/MainActivity.kt
+++ app/src/main/java/com/newsapp/MainActivity.kt
@@ -2,10 +2,22 @@ package com.newsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Adapter
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    private val newsList = mutableListOf<News>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initialRecView()
+    }
+
+    private fun initialRecView(){
+        val recView = findViewById<RecyclerView>(R.id.recView)
+        val newsAdopter = NewsAdopter(newsList)
+        recView.adapter = newsAdopter
+        recView.layoutManager = LinearLayoutManager(this)
     }
 }
